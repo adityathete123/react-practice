8a3db5f9f8e0ec3def85d2d3ff3c53800d21b5ef
Aditya Thete <theteaditya17@gmail.com>
2026-01-02 10:05:33 +0530

    01-Folder-Done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+const App = () => {
+  return (
+   <div id="parent">
+    <div id="aditya">
+      <h1>in aditya</h1>
+    </div>
+    <div id="harsh">
+      <h1>In Harsh</h1>
+    </div>
+   </div>
+  )
+}
+
+export default App
